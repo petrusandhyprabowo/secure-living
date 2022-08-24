22fa7ac2461cdc7adb28b60579a6aea3214b81be
Petrus Andhy Prabowo <qwer.ty191110@gmail.com>
2022-08-24 14:02:49 +0700

    update connection to new db heroku

--- src/config/dbcon.py
+++ src/config/dbcon.py
@@ -1,10 +1,15 @@
 import psycopg2
 
 # heroku
-host = 'ec2-23-23-151-191.compute-1.amazonaws.com'
-database = 'd8gk809gjhcnkd'
-user = 'omjmbqihmwzrqm'
-password = '84469b88500b335b2da4991b32154196c194edd18368a07f6615a6cab2b61f1d'
+host = 'ec2-44-205-41-76.compute-1.amazonaws.com'
+database = 'd7hsb5uqdla0c8'
+user = 'yxyixqraducmzw'
+password = '2c1db32e1043197131cf748d399e82cb755810edc159c0ed3d2eeda9da4cef94'
+
+# host = 'ec2-23-23-151-191.compute-1.amazonaws.com'
+# database = 'd8gk809gjhcnkd'
+# user = 'omjmbqihmwzrqm'
+# password = '84469b88500b335b2da4991b32154196c194edd18368a07f6615a6cab2b61f1d'
 
 # local
 # host = 'localhost'
